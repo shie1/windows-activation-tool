22a3c170e1f3ae65c15eed9b8f9b52eb77c59497
SussusAmogsus <64425307+SussusAmogsus@users.noreply.github.com>
2021-11-19 15:59:36 +0000

    Update nopopups.py

--- nopopups.py
+++ nopopups.py
@@ -3,17 +3,17 @@ from os import system
 from threading import Thread
 from sys import exit
 
-i = True
+stop_flag = True
 
 def stop():
-    global i
-    i = False
+    global stop_flag
+    stop_flag = False
 
 def __main():
-    global i
-    while i == True:
+    global stop_flag
+    while stop_flag:
         try: system("taskkill /f /im wscript.exe >nul 2>&1")
-        except: ""
+        except: pass
         sleep(.25)
     exit()
         
